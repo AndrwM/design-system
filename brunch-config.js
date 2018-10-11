b3bbd8d2dbef62276291eaaacb387b328593a915
Andrew McIntee <contact@andrew.mn>
2018-10-11 14:45:19 -0400

    Basic project structure, migrate SC Codes components

---
+++ brunch-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  config: {
+    paths: {
+      watched: ["design-system"],
+    },
+    files: {
+      stylesheets: {
+        joinTo: { 'assets/style.css': 'design-system/manifest.scss'}
+      }
+    },
+    plugins: {
+      sass: {
+        mode: "native"
+      }
+    }
+  }
+};
